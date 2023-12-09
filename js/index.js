62141006c11358cc064b414f9eb493534050c88f
Sehee Yoon <ysehee23@gmail.com>
2023-12-09 23:09:38 +0900

    윤세희 종합 포트폴리오 최종 완성

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 
+// HOME 화면에서 "안녕하세요~"" 메세지 차례대로 띄우는 기능
 function changeText(cont1, cont2, speed, callback) {
     var Otext = cont1.text();
     var Ocontent = Otext.split("");
@@ -28,7 +29,7 @@ $(document).ready(function () {
 
 
 
-
+// contact 영역에서 이메일 아이콘 눌렀을 때
 $(function(){    
     var clipboard = new ClipboardJS('#copyEmail');
     clipboard.on('success', function(e) {
@@ -39,6 +40,7 @@ $(function(){
     });
 });
 
+// contact 영역에서 카카오 아이콘 눌렀을 때
 $(function(){    
     var clipboard = new ClipboardJS('#copyKakaoId');
     clipboard.on('success', function(e) {
@@ -50,10 +52,57 @@ $(function(){
 });
 
 
+// 팀 프로젝트 깃허브 주소로 이동
 function goGithub()  {
     window.open('https://github.com/ysehee/BlueHorizon.git') ;
 }
 
+// 프론트엔드 프로젝트 (첫 플젝) 깃허브 주소로 이동
 function goGithub2()  {
     window.open('https://github.com/ysehee/HAGO_Front_Portfolio.git') ;
 }
+
+// skills 영역 게이지 바 채워지기
+
+$(document).ready(function(){
+    let skill_length = $('.skills').length;
+    let s_top = '';
+
+////////////////////////////
+
+function tmp(i, num) {
+    let up = 0;let interval;
+    interval=setInterval(function(){
+        up++; 
+        
+        $('.skills').eq(i).find('.skill_color').css({
+            width: up + "%"
+        })
+        $('.skills').eq(i).find('.percent').text(up);
+        if(up >= num) {
+            clearInterval(interval);
+        }
+    }, 5)
+}
+///////////////////////////////////////
+    let sec1_chk = true;
+    let sec3_chk = true;
+    $(window).scroll(function(){ 
+        //헤더픽스
+        s_top = $(window).scrollTop();
+        console.log(s_top);
+        
+        //퍼센트
+        let sec3_top = $('#skills').offset().top;    
+        if(sec3_top - 300 <= s_top) 
+            if(sec3_chk) {
+                sec3_chk = false;{
+                let array = [95,95,90,85,90,90,95,90,90,90,85,90];
+                for(let i=0; i<skill_length; i++) {
+                    tmp(i, array[i]) ;
+                } 
+            }
+        }
+    });
+});
+
